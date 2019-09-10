f150b6f231eecaa8045587018c57d499d7b942d6
ymjrchx <ymjrchx@yeah.net>
2019-09-11 00:08:58 +0800

    xig

---
+++ trendParentProject/index-gather-store-service/src/main/java/cn/how2j/trend/web/IndexController.java
@@ -0,0 +1,23 @@
+package cn.how2j.trend.web;
+
+import cn.how2j.trend.pojo.Index;
+import cn.how2j.trend.service.IndexService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class IndexController {
+    @Autowired
+    IndexService indexService;
+
+    @GetMapping("/getCodes")
+    public List<Index> get(){
+     return indexService.fetch_indexes_from_third_part();
+    }
+
+
+
+}
