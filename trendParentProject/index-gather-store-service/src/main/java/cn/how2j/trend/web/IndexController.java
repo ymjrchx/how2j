92ea45db55ad2c9877924d66185ced143b2b94ff
ymjrchx <ymjrchx@yeah.net>
2019-09-15 12:32:07 +0800

    xig

--- trendParentProject/index-gather-store-service/src/main/java/cn/how2j/trend/web/IndexController.java
+++ trendParentProject/index-gather-store-service/src/main/java/cn/how2j/trend/web/IndexController.java
@@ -13,9 +13,23 @@ public class IndexController {
     @Autowired
     IndexService indexService;
 
+
+//  http://127.0.0.1:8001/freshCodes
+//  http://127.0.0.1:8001/getCodes
+//  http://127.0.0.1:8001/removeCodes
+
+    @GetMapping("/freshCodes")
+    public List<Index> fresh() throws Exception {
+        return indexService.fresh();
+    }
     @GetMapping("/getCodes")
-    public List<Index> get(){
-     return indexService.fetch_indexes_from_third_part();
+    public List<Index> get() throws Exception {
+        return indexService.get();
+    }
+    @GetMapping("/removeCodes")
+    public String remove() throws Exception {
+        indexService.remove();
+        return "remove codes successfully";
     }
 
 
